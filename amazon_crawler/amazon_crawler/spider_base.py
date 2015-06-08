04a8c2011e23edaecde851699a9326ea1993dbe6
bobibi <bobibi@users.noreply.github.com>
2015-06-08 11:33:20 -0700

    Update spider_base.py

--- amazon_crawler/amazon_crawler/spider_base.py
+++ amazon_crawler/amazon_crawler/spider_base.py
@@ -3,60 +3,23 @@ import scrapy
 import sys
 from scrapy.selector import Selector 
 import amazon_crawler.spider_logger as db_logger 
-from amazon_crawler.items import AmazonItem as ReviewerItem
 import amazon_crawler.mysql_helper as db
 import amazon_crawler.html_extractor as html_extractor
 from cssselect import GenericTranslator, SelectorError
 from scrapy import log
 from scrapy import exceptions
 import re
+from abc import ABCMeta
 
-
-class ProductSpider(scrapy.Spider):
-    name = "reviewer"
-    html_page = 'AmazonReviewer'
-    allowed_domains = ["amazon.com"]
+class SpiderBase(scrapy.Spider):
+    __metaclass__ = ABCMeta
     
-    def __init__(self, *args, **kwargs):
-        if not kwargs.has_key('uid') or not re.match('^[0-9A-Z]{10,24}(,[0-9A-Z]{10,24})*$', kwargs.get('uid')):
-            log.msg('missing or invalid param uid', level=log.ERROR)
-            raise exceptions.CloseSpider('missing or invalid param uid, please use "-a uid=<<Amazon User ID>>"')
-        self.url_template = db.get_crawler_setting(self.html_page, 'UrlTemplate')
-        if not self.url_template:
-            log.msg('cannot find url template in crawler setting from database', level=log.ERROR)
-            raise exceptions.CloseSpider('cannot find url template in crawler setting from database')
-        self.start_urls = [re.sub('<<UID>>', a, self.url_template) for a in kwargs.get('uid').split(',')]
-
-    def parse(self, response):
-        item = ReviewerItem()
-        if response.status != 200:
-            db_log('url(%s) response code is %d, 200 is expected'%(response.url, response.status), 
-                   lv='error', )
-            item['success'] = False
-            return item
-        
-        m = re.search('\/([0-9A-Z]{10,24})(?![0-9A-Z])', response.url)
-        if not m:
-            db_log('cannot parse uid from response url: %s'%response.url, lv='error', spider=self.name)
-            log.msg('cannot parse uid from response url: %s'%response.url, level=log.ERROR)
-            raise exceptions.CloseSpider('cannot parse uid from response url:%s'%response.url)
-        uid = m.group(1)
-        
-        sel = Selector(response)
-        extractor_list = db.get_page_extractor_list(self.html_page)
-        if not extractor_list:
-            db_log(message = 'no extractor for Page=%s, refer to table HtmlExtractor'%self.html_page,
-                   lv = 'fatal',spider = self.name)
-        
-        extract_result = html_extractor.extract(sel, extractor_list, self.name, uid)
-        
-        if extract_result['mismatch']:
-            raise exceptions.CloseSpider('some required fields are not extracted correctely due to missing selector, detail is in database')
-        
-        reviewer = extract_result['data']
-        reviewer[u'UID'] = uid
-        #reviewer[u'URL'] = response.url
-        
-        item['data'] = reviewer
-        item['success'] = True
-        return item
+    def get_arg(self, *args, **kwargs, name):
+        arg_setting = db.get_crawler_setting(self.html_page, name)
+        if not arg_setting:
+            log.msg('missing configuration setting for arg: %s, refer to db table CrawlerSetting'%name, level=log.ERROR)
+            raise exceptions.CloseSpider('missing configuration setting for arg: %s, refer to db table CrawlerSetting'%name)
+        if not kwargs.has_key(name) or not re.match(arg_setting.Value, kwargs.get(name)):
+            log.msg('missing or invalid param %s'%name, level=log.ERROR)
+            raise exceptions.CloseSpider('missing or invalid param %s, please use "-a %s=<<%s>>"'%(name,name,name))
+        setattr(self, name, kwargs.get(name))
