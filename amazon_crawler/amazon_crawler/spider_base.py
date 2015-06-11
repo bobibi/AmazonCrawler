fe5634b5b6ebbf8eb9dab1fa8193f6c9c17560ee
bobibi <bobibi@users.noreply.github.com>
2015-06-11 11:30:53 -0700

    Update spider_base.py

--- amazon_crawler/amazon_crawler/spider_base.py
+++ amazon_crawler/amazon_crawler/spider_base.py
@@ -18,12 +18,19 @@ class SpiderBase(scrapy.Spider):
         if kwargs.has_key('debug') and kwargs.get('debug').lower()=='on':
             self.debug = True
     
-    '''def get_arg(self, *args, **kwargs, name):
-        arg_setting = db.get_crawler_setting(self.html_page, name)
+    def require_arg(self, *args, **kwargs, name):
+        '''
+        This method will get argument from command line which is provided by using "-a arg=val"
+        If the argument is not found, an exception is raised
+        It will retrieve the regexp from database setting table, and use it to match the coming arg
+        If the matching failed, an exception will be raised
+        If no regexp found, exception too
+        '''
+        arg_setting = db.get_crawler_setting(self.html_page, 'cl_arg.'+name+'.regexp')
         if not arg_setting:
             log.msg('missing configuration setting for arg: %s, refer to db table CrawlerSetting'%name, level=log.ERROR)
             raise exceptions.CloseSpider('missing configuration setting for arg: %s, refer to db table CrawlerSetting'%name)
         if not kwargs.has_key(name) or not re.match(arg_setting.Value, kwargs.get(name)):
             log.msg('missing or invalid param %s'%name, level=log.ERROR)
             raise exceptions.CloseSpider('missing or invalid param %s, please use "-a %s=<<%s>>"'%(name,name,name))
-        setattr(self, name, kwargs.get(name))'''
+        setattr(self, name, kwargs.get(name))
